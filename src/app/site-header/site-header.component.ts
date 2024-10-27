e9cd34fd38aee09d691c199762187ada50df1c99
ajaybhalerao12 <ajaybhalerao12@gmail.com>
2024-10-27 11:47:36 +0530

    feat: create site header component

---
+++ src/app/site-header/site-header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css']
+})
+export class SiteHeaderComponent {
+
+  constructor() { }
+
+}
