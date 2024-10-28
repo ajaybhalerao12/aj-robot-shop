7b3b2c62426b76e8ab7b754c5f01802b769c986b
ajaybhalerao12 <ajaybhalerao12@gmail.com>
2024-10-28 19:55:05 +0530

    feat: provideHttpClient, product and cart services, Node.js API
    - Added provideHttpClient to Angular app for HTTP client services
    - Created ProductService to fetch product data
    - Created CartService to manage items in the cart
    - Developed a Node.js API server
      - Added endpoints to return products
      - Added endpoints to return products added to the cart
      - Added endpoints to add products to the cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 interface ILineItem {
   id: number;
@@ -11,37 +12,42 @@ interface ILineItem {
   providedIn: 'root',
 })
 export class CartService {
-  private cart: ILineItem[] = [];
+  // private cart: ILineItem[] = [];
+  private cart: IProduct[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  getTotalPrice() {
-    return (
-      Math.round(
-        this.cart.reduce<number>((prev, cur) => {
-          return (
-            prev + cur.qty * (cur.product.price * (1 - cur.product.discount))
-          );
-          0;
-        }, 0) * 100
-      ) / 100
-    );
-  }
+  // getTotalPrice() {
+  //   return (
+  //     Math.round(
+  //       this.cart.reduce<number>((prev, cur) => {
+  //         return (
+  //           prev + cur.qty * (cur.product.price * (1 - cur.product.discount))
+  //         );
+  //         0;
+  //       }, 0) * 100
+  //     ) / 100
+  //   );
+  // }
 
-  findLineItem(product:IProduct) {
-    return this.cart.find((lineItem) => lineItem.product.id === product.id);
-  }
+  // findLineItem(product:IProduct) {
+  //   return this.cart.find((lineItem) => lineItem.product.id === product.id);
+  // }
 
   add(product: IProduct) {
-    let lineItem = this.findLineItem(product);
-    if(lineItem != undefined){
-      lineItem.qty++;
-    }else{
-      lineItem = { product: product, qty: 1, id: product.id };
-      this.cart.push(lineItem);
-    }
-    console.log(`Product with id ${product.id} added to cart`);
-    console.log(`Total price: ${this.getTotalPrice()}`);
+    this.cart.push(product);
+    this.http.post('/api/cart', this.cart).subscribe(()=>{
+      console.log(`Product with id ${product.id} added to cart`);
+    });
+    // let lineItem = this.findLineItem(product);
+    // if(lineItem != undefined){
+    //   lineItem.qty++;
+    // }else{
+    //   lineItem = { product: product, qty: 1, id: product.id };
+    //   this.cart.push(lineItem);
+    // }
+    // console.log(`Product with id ${product.id} added to cart`);
+    // console.log(`Total price: ${this.getTotalPrice()}`);
     // this.cart.push(product);
     // console.log(`Product with id ${product.id} added to cart`);
   }
